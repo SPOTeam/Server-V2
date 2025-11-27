bebb23a5ce8d8dd23df9af5efb621f05f9396107
msk226 <kminseok5167@gmail.com>
2025-11-27 15:51:06 +0900

    [Feature] 스터디 신청 관련 API 시그니처 작성

---
+++ modules/study/src/main/java/kr/spot/presentation/query/dto/response/GetAppliesResponse.java
@@ -0,0 +1,21 @@
+package kr.spot.presentation.query.dto.response;
+
+import java.util.List;
+
+public record GetAppliesResponse(List<Apply> applies) {
+
+    public static GetAppliesResponse of(List<Apply> applies) {
+        return new GetAppliesResponse(applies);
+    }
+
+    public record Apply(
+            Long applicantId,
+            String nickname,
+            String description,
+            String profileImageUrl
+    ) {
+        public static Apply of(Long applicantId, String nickname, String description, String profileImageUrl) {
+            return new Apply(applicantId, nickname, description, profileImageUrl);
+        }
+    }
+}
