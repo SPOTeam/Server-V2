36e8e9c02bb3db81220984ff9dd25d575588d3c7
msk226 <kminseok5167@gmail.com>
2025-12-09 13:21:16 +0900

    [Feature] 알림 도메인 엔티티 정의

--- modules/notification/src/main/java/kr/spot/domain/Notification.java
+++ modules/notification/src/main/java/kr/spot/domain/Notification.java
@@ -1,5 +1,37 @@
 package kr.spot.domain;
 
-public class Notification {
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import kr.spot.domain.vo.Content;
+import kr.spot.domain.vo.NotificationTarget;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
+@Getter
+@Entity
+@SQLDelete(sql = "UPDATE notification SET status = 'INACTIVE' WHERE id = ?")
+@SQLRestriction("status = 'ACTIVE'")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Notification extends BaseEntity {
+
+  @Id
+  private Long id;
+
+  @Embedded
+  private Content content;
+
+  @Embedded
+  private NotificationTarget target;
+
+  private Boolean isChecked;
+
+  public static Notification of(Long id, Content content, NotificationTarget target) {
+    return new Notification(id, content, target, false);
+  }
 }
