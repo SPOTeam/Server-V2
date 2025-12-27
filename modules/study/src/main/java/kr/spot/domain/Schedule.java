3b98320b1c8eb02c75f23ada6f331d1e57a5fac2
msk226 <kminseok5167@gmail.com>
2025-12-27 14:49:30 +0900

    [Feature] 스터디 일정 도메인 엔티티 정의

---
+++ modules/study/src/main/java/kr/spot/domain/Schedule.java
@@ -0,0 +1,42 @@
+package kr.spot.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Entity
+@SQLDelete(sql = "UPDATE schedule SET status = 'INACTIVE' WHERE id = ?")
+@SQLRestriction("status = 'ACTIVE'")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Schedule extends BaseEntity {
+
+  @Id
+  private Long id;
+
+  private Long studyId;
+
+  @Column(nullable = false)
+  private String title;
+
+  private String locationMemo;
+
+  private LocalDateTime startAt;
+
+  private LocalDateTime endAt;
+
+  public static Schedule of(Long id, Long studyId, String title, String locationMemo,
+      LocalDateTime startAt, LocalDateTime endAt
+  ) {
+    return new Schedule(id, studyId, title, locationMemo, startAt, endAt
+    );
+  }
+}
