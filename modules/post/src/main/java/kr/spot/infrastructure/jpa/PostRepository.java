c20c1cc0fcceb7a338f6fb0827ffa50cb6281ddf
msk226 <kminseok5167@gmail.com>
2025-12-30 14:32:34 +0900

    [Feature] 게시글 신고 API 구현

--- modules/post/src/main/java/kr/spot/infrastructure/jpa/PostRepository.java
+++ modules/post/src/main/java/kr/spot/infrastructure/jpa/PostRepository.java
@@ -19,9 +19,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   @Query("select p from Post p where p.id = :id")
   Optional<Post> findByIdWithLock(long id);
 
-  @Query("select p from Post p where p.postType = :postType order by p.id desc")
-  Optional<Post> findTopByPostTypeOrderByIdDesc(PostType postType);
-
   @Modifying
   @Query("""
         update Post p
@@ -61,5 +58,11 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
   default Post getPostById(long id) {
     return findById(id).orElseThrow(() -> new GeneralException(ErrorStatus._POST_NOT_FOUND));
+  }
+
+  default void validateExists(long postId) {
+    if (!existsById(postId)) {
+      throw new GeneralException(ErrorStatus._POST_NOT_FOUND);
+    }
   }
 }
