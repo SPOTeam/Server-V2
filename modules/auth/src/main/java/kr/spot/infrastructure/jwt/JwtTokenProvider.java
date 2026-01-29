4481057bae4c29fef4dd10857e0ea801fbfbb6c0
msk226 <kminseok5167@gmail.com>
2026-01-29 16:50:46 +0900

    [Feature] 소셜 로그인 성공 시, 회원 ID 함께 응답

--- modules/auth/src/main/java/kr/spot/infrastructure/jwt/JwtTokenProvider.java
+++ modules/auth/src/main/java/kr/spot/infrastructure/jwt/JwtTokenProvider.java
@@ -49,7 +49,7 @@ public class JwtTokenProvider implements TokenProvider {
         REFRESH); // 리프레시 토큰 생성
 
     // 토큰 DTO 반환
-    return TokenDTO.of(accessToken, refreshToken);
+    return TokenDTO.of(memberId, accessToken, refreshToken);
   }
 
   private String generateToken(final Long memberId, Date now, final long expirationTime,
@@ -92,7 +92,7 @@ public class JwtTokenProvider implements TokenProvider {
     long oneYearInMillis = 365L * 24 * 60 * 60 * 1000;
     String accessToken = generateToken(memberId, now, oneYearInMillis, ACCESS);
     String refreshToken = generateToken(memberId, now, oneYearInMillis, REFRESH);
-    return TokenDTO.of(accessToken, refreshToken);
+    return TokenDTO.of(memberId, accessToken, refreshToken);
   }
 
   private Claims getClaims(final String token) {
